e47d621de5fb7ae6d076b50eb25c84cca5f4c680
Chitra2409 <schitra756@gmail.com>
2024-08-14 17:28:57 +0530

    minor changed

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1plxvST3dAXji4Kkvh8PkkVFxdQ3Yg3k",
-  authDomain: "flashcard-saas-ee680.firebaseapp.com",
-  projectId: "flashcard-saas-ee680",
-  storageBucket: "flashcard-saas-ee680.appspot.com",
-  messagingSenderId: "530985263179",
-  appId: "1:530985263179:web:626eb3276bfe825fbbadce",
-  measurementId: "G-9YJYTCP1FM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
